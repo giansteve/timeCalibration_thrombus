79aaef711e272158dd7f68a1fadce5ed3d7049fa
Melito <gm20m18@tugraz.at>
2021-12-10 10:43:38 +0100

    Surrogate accuracy verification: added plot of the probabilities

---
+++ GM_pdf_matrix.m
@@ -0,0 +1,37 @@
+function [] = GM_pdf_matrix(mat)
+% generate probability distribution function (pdf) of a matrix. The second
+% 1ension is intended as time, therefore the analysis of the PDF is
+% dynamic
+% the "time" variable has to be in position 1, e.g. mat[t,any]
+
+%% define colors
+colors = zeros(size(mat,1),3);
+for i = 1:size(mat,1)
+    colors(i,:) = [1-i/size(mat,1) 1-i/size(mat,1) 1-i/size(mat,1)];
+end
+colors(colors==0)=.05;
+
+%% define max and min of variable
+N_edges = 100;
+edges_hist = linspace(min(min(mat)),max(max(mat)),N_edges);
+centres = edges_hist(1:end-1)+ diff(edges_hist)/2;
+
+% figure
+hold on
+for i = 1:size(mat,1)
+    h = histcounts(mat(i,:),edges_hist,'Normalization','probability');
+    plot(centres,smooth(h),'Color',colors(i,:))
+end
+xlim([min(edges_hist) max(edges_hist)])
+grid on
+
+
+
+
+
+
+
+
+
+
+end
